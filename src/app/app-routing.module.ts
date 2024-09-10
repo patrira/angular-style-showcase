9578e63974a7849d0c1924bab8519df96070da3d
patrira <iradukundapatrick19@gmail.com>
2024-09-11 01:25:33 +0200

    Fixing responsiveness

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
-const routes: Routes = [{ path: 'car-category', loadChildren: () => import('./modules/car-category/car-category.module').then(m => m.CarCategoryModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },  
+  { path: 'home', component: MainContentComponent },     
+  { path: 'cars', loadChildren: () => import('./modules/car-category/car-category.module').then(m => m.CarCategoryModule) }, // Lazy-loaded route
+  { path: '**', redirectTo: '/home' }  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
