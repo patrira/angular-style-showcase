bf41a8675502fc1e550b17151407981a38681da2
patrira <iradukundapatrick19@gmail.com>
2024-09-10 22:32:24 +0200

    Fixing errors

--- src/app/components/main-content/car-card/car-card.component.ts
+++ src/app/components/main-content/car-card/car-card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Car } from '../../../services/car.service';  
 
 @Component({
   selector: 'app-car-card',
   templateUrl: './car-card.component.html',
-  styleUrl: './car-card.component.scss'
+  styleUrls: ['./car-card.component.scss']
 })
 export class CarCardComponent {
-
+  @Input() car!: Car;  
 }
