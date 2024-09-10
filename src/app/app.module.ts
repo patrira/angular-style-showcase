2bbba9e2df71392289c5ab7e8ff09da9813e1798
patrira <iradukundapatrick19@gmail.com>
2024-09-10 21:47:39 +0200

    Project Folder Structuring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { CarCardComponent } from './components/main-content/car-card/car-card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainContentComponent,
+    CarCardComponent,
+    FooterComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
