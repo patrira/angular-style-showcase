8ffbbb4c2681c98c68ae74d67d703b67f34560fc
patrira <iradukundapatrick19@gmail.com>
2024-09-11 11:05:13 +0200

    Handling lazy loading

--- src/app/modules/car-category/car-category.component.ts
+++ src/app/modules/car-category/car-category.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarService, Car } from '../../services/car.service';  // Import CarService and Car model
 
 @Component({
   selector: 'app-car-category',
   templateUrl: './car-category.component.html',
-  styleUrl: './car-category.component.scss'
+  styleUrls: ['./car-category.component.scss']
 })
-export class CarCategoryComponent {
+export class CarCategoryComponent implements OnInit {
+  cars: Car[] = [];
 
+  constructor(private carService: CarService) { }
+
+  ngOnInit(): void {
+    this.cars = this.carService.getCars();  // Fetch the car data from the service
+  }
 }
